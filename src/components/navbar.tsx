0f5126f76e64d95345d1799a32c677a7690e0702
mgkmoha <mgkmoha@gmail.com>
2023-08-03 17:50:20 +0530

    added react firebase hook library 4 user acnt syn

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom";
 import { auth } from "../config/firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const Navbar = () => {
+
+    const [user] = useAuthState(auth);
+
     return (
     <div>
         <Link to="/"> Home </Link>
         <Link to="/login"> Login </Link>
 
         <div>
-            <p>{auth.currentUser?.displayName}</p>
-            <img src={auth.currentUser?.photoURL || ""} width="100" height="100"/>
+            <p>{user?.displayName}</p>
+            <img src={user?.photoURL || ""} width="20" height="20"/>
         </div>
     </div>
     
